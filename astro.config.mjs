177b55b38f510f76ef6dd72aa8b8f0e133481ab8
Billy Kaplan <billy1kaplan@gmail.com>
2025-06-07 12:44:13 -0400

    Fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "billykaplan.io",
+	site: "https://billykaplan.io",
 	base: "blog",
 	integrations: [mdx(), sitemap()],
 });
